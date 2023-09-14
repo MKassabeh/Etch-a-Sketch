fde36b612b93901940f7356b72bd42c1703ce8bb
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-09-14 11:21:41 +0200

    End of Etch a sketch

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const container = document.getElementById("container");
+let container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = "";
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+let clear = document.getElementById("gnBtn")
 
-makeRows(16, 16);
+let blackAndWhite = document.getElementById("blAndWh");
+// Function to have a random color
 
 function randomRgbColor(){
   let r = Math.floor(Math.random() * 256); // Random between 0-255
@@ -20,19 +13,60 @@ function randomRgbColor(){
 };
 
 
-const cells = document.querySelectorAll(".grid-item");
-
-
-
-cells.forEach((e)=> {
-
-  e.addEventListener("mouseover", (el) => {
-
-    
-    el.target.style.backgroundColor =randomRgbColor();
-  
-  })
+clear.addEventListener('click', ()=>{ // we only want to add this listener once
+    resetSize()
 })
+
+function resetSize(){
+        
+  let number = prompt("What size would you like the grid to be? (1-100)");
+  container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+  makeRows(number); // call the createGrid function here and pass the number they entered as the argument. 
+}
+
+
+function makeRows(size) {
+
+  
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+
+  for (c = 0; c < size*size; c++) {
+
+      let cell = document.createElement("div");
+      cell.style.backgroundColor="black"
+      container.appendChild(cell);
+
+      cell.addEventListener("mouseover", el => {
+        cell.style.backgroundColor = randomRgbColor();     
+      })  
+      
+      blackAndWhite.addEventListener("click", el => {
+        clearGrid()
+        cell.addEventListener("mouseover", el => {
+          if(cell.backgroundColor==="white"){
+            cell.style.backgroundColor = "black"
+          }else{
+            cell.style.backgroundColor = "white"
+          }
+
+        })
+        
+      })
+    function clearGrid(){
+      clear.addEventListener('click', e=>{
+          cell.style.backgroundColor = "black"
+      })
+    } 
+
+    clearGrid(); 
+  };
+}
+
+
+makeRows(16);
 
 
 
